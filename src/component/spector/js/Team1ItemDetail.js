3a9406c13b0558b9801ca91b665ad3354f381da0
GaRyung <shubom0115@naver.com>
2024-01-17 17:28:31 +0900

    li 태그 줄임

--- src/component/spector/js/Team1ItemDetail.js
+++ src/component/spector/js/Team1ItemDetail.js
@@ -22,66 +22,7 @@ const Team1ItemDetail = () => {
                         </div>
 
                     </li>
-                    <li className={'t1-member'}>
-                        <div className={'t1-tier'}><a></a></div>
-                        <div className={'t1-name'}>
-                            <a className={'nickname'}>name</a>
-                            <a className={'tag'}>#tag</a>
-                        </div>
-                        <div className={'t1-img'}>
-                            <div className={'spell-img-box'}>
-                                <div className={'spell-img'}></div>
-                                <div className={'spell-img'}></div>
-                            </div>
-                            <div className={'user-img'}></div>
-                        </div>
-
-                    </li>
-                    <li className={'t1-member'}>
-                        <div className={'t1-tier'}><a></a></div>
-                        <div className={'t1-name'}>
-                            <a className={'nickname'}>name</a>
-                            <a className={'tag'}>#tag</a>
-                        </div>
-                        <div className={'t1-img'}>
-                            <div className={'spell-img-box'}>
-                                <div className={'spell-img'}></div>
-                                <div className={'spell-img'}></div>
-                            </div>
-                            <div className={'user-img'}></div>
-                        </div>
-
-                    </li>
-                    <li className={'t1-member'}>
-                        <div className={'t1-tier'}><a></a></div>
-                        <div className={'t1-name'}>
-                            <a className={'nickname'}>name</a>
-                            <a className={'tag'}>#tag</a>
-                        </div>
-                        <div className={'t1-img'}>
-                            <div className={'spell-img-box'}>
-                                <div className={'spell-img'}></div>
-                                <div className={'spell-img'}></div>
-                            </div>
-                            <div className={'user-img'}></div>
-                        </div>
-
-                    </li>
-                    <li className={'t1-member'}>
-                        <div className={'t1-tier'}><a></a></div>
-                        <div className={'t1-name'}>
-                            <a className={'nickname'}>name</a>
-                            <a className={'tag'}>#tag</a>
-                        </div>
-                        <div className={'t1-img'}>
-                            <div className={'spell-img-box'}>
-                                <div className={'spell-img'}></div>
-                                <div className={'spell-img'}></div>
-                            </div>
-                            <div className={'user-img'}></div>
-                        </div>
-
-                    </li>
+                    
                 </ul>
             </div>
         </div>
