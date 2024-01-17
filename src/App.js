5723ec29a12be35d09d045d0336eee8348524ffa
wonhwo <dlgy9714@gmail.com>
2024-01-17 17:50:20 +0900

    보더 열심히 하는중...

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
             <Route path="/board" element={<Board />}/>
         </Routes>
       </BrowserRouter>
-
   );
 }
 
--- src/component/board/js/Board.js
+++ src/component/board/js/Board.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "../../header/js/Header";
 import "../scss/board.scss";
+import {CiSearch} from "react-icons/ci";
+import { GoChevronDown } from "react-icons/go";
+import cn from "classnames";
 const Board = () => {
+    const [hide,setHide]=useState(false);
+    const [title,setTitle]=useState("제목");
+
+    const relativeButtonHandler=()=>{
+        setHide(!hide);
+    }
+    const hiddenHandler=(e)=>{
+        setTitle(e.target.value);
+    }
     return (
         <>
             <Header/>
@@ -9,16 +21,42 @@ const Board = () => {
                 <div id="board_header"></div>
                 <section id="board_main">
                     <div className="board_nav_bar">
-                        <a className="WritePost">게시글 작성</a>
-                        <ul className="nav_bar_listBox">
-                            <li>자유게시판</li>
-                            <li>LCK 게시판</li>
-                            <li>공략게시판</li>
-                            <li>하이라이트</li>
-                        </ul>
+                        <div className="aside">
+                            <a className="WritePost">게시글 작성</a>
+                            <ul className="nav_bar_listBox">
+                                <li className="nav_bar_list click">자유게시판</li>
+                                <li className="nav_bar_list">LCK 게시판</li>
+                                <li className="nav_bar_list">공략게시판</li>
+                                <li className="nav_bar_list">하이라이트</li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="board_list_box">
-                        <div className="board_search_box"></div>
+                        <div className="board_search_box">
+                            <div className="toggleBtn">
+                                <button className="sorting-button toggle">최신글</button>
+                                <button className="sorting-button">인기글</button>
+                            </div>
+                            <div className="searchT">
+                                <div className="relative">
+                                    <button className="relativeBtn" onClick={relativeButtonHandler}>
+                                        <span>{title}</span>
+                                        <GoChevronDown size={12*2}/>
+                                        <div className={cn("hiddenSelect",{hide:hide})}>
+                                            <ul>
+                                                <li><button onClick={hiddenHandler} value="제목">제목</button></li>
+                                                <li><button onClick={hiddenHandler} value="제목 + 내용">제목 + 내용</button></li>
+                                                <li><button onClick={hiddenHandler} value="작성자">작성자</button></li>
+                                            </ul>
+                                        </div>
+                                    </button>
+                                </div>
+                                <form>
+                                    <input/>
+                                    <button><CiSearch/></button>
+                                </form>
+                            </div>
+                        </div>
                         <div className="board_table_box">
                             <table>
                                 <tr>
