7249f26d1792adc22b5651bfa0369bd33707f32c
GaRyung <shubom0115@naver.com>
2024-01-17 16:53:01 +0900

    모달창 틀 진짜 완성

--- src/component/spector/js/Team2ItemDetail.js
+++ src/component/spector/js/Team2ItemDetail.js
@@ -21,6 +21,62 @@ const Team2ItemDetail = () => {
                         </div>
                         <div className={'t2-tier'}><a></a></div>
                     </li>
+                    <li className={'t2-member'}>
+                        <div className={'t2-img'}>
+                            <div className={'user-img'}></div>
+                            <div className={'spell-img-box'}>
+                                <div className={'spell-img'}></div>
+                                <div className={'spell-img'}></div>
+                            </div>
+                        </div>
+                        <div className={'t2-name'}>
+                            <a className={'nickname'}>name</a>
+                            <a className={'tag'}>#tag</a>
+                        </div>
+                        <div className={'t2-tier'}><a></a></div>
+                    </li>
+                    <li className={'t2-member'}>
+                        <div className={'t2-img'}>
+                            <div className={'user-img'}></div>
+                            <div className={'spell-img-box'}>
+                                <div className={'spell-img'}></div>
+                                <div className={'spell-img'}></div>
+                            </div>
+                        </div>
+                        <div className={'t2-name'}>
+                            <a className={'nickname'}>name</a>
+                            <a className={'tag'}>#tag</a>
+                        </div>
+                        <div className={'t2-tier'}><a></a></div>
+                    </li>
+                    <li className={'t2-member'}>
+                        <div className={'t2-img'}>
+                            <div className={'user-img'}></div>
+                            <div className={'spell-img-box'}>
+                                <div className={'spell-img'}></div>
+                                <div className={'spell-img'}></div>
+                            </div>
+                        </div>
+                        <div className={'t2-name'}>
+                            <a className={'nickname'}>name</a>
+                            <a className={'tag'}>#tag</a>
+                        </div>
+                        <div className={'t2-tier'}><a></a></div>
+                    </li>
+                    <li className={'t2-member'}>
+                        <div className={'t2-img'}>
+                            <div className={'user-img'}></div>
+                            <div className={'spell-img-box'}>
+                                <div className={'spell-img'}></div>
+                                <div className={'spell-img'}></div>
+                            </div>
+                        </div>
+                        <div className={'t2-name'}>
+                            <a className={'nickname'}>name</a>
+                            <a className={'tag'}>#tag</a>
+                        </div>
+                        <div className={'t2-tier'}><a></a></div>
+                    </li>
                 </ul>
             </div>
         </div>
