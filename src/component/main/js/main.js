e8f3df5ae655fce7cc165ce129fea0b5e646734c
yongseopK <kk0@kakao.com>
2024-01-18 14:24:24 +0900

    main페이지 백엔드처리 완료
    summonerInfo 페이지 생성

--- src/component/main/js/main.js
+++ src/component/main/js/main.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../header/js/Header";
 import '../scss/main.scss';
 import {FaSearch} from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
+
+const Input = ({onSubmit, ...rest})=> {
+    return (
+        <form onSubmit={onSubmit} className={"input-form"} >
+            <input {...rest} className={"title"} placeholder={"소환사명을 입력해주세요"}/>
+            <button><FaSearch className='a'/></button>
+        </form>
+    )
+}
 const Main = () => {
+
+    const [inputValue, setInputValue] = useState('');
+    const [splitValue, setSplitValue] = useState(['', 'KR1']);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(splitValue[0] !== '') {
+            console.log(splitValue);
+            navigate(`/find/${splitValue[0]}/${splitValue[1]}`);
+        }
+    }, [splitValue, navigate]);
+
+    const handleChange = e => {
+        setInputValue(e.target.value);
+    };
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      const splitInput = inputValue.split('#');
+      if(!splitInput[0]) {
+          alert('소환사명을 입력해주세요.');
+          return;
+      }
+      setSplitValue([splitInput[0], splitInput[1] ? splitInput[1] : 'KR1']);
+    };
+
     return (
         <>
             <Header/>
@@ -11,8 +47,8 @@ const Main = () => {
                     <a href='#' className='title__logo'></a>
                 </div>
                 <div className='input__Title'>
-                    <input className='title' placeholder='소환사명을 입력해주세요'/>
-                    <FaSearch className='a'/>
+                    <Input value={inputValue} onChange={handleChange} onSubmit={handleSubmit}/>
+                    {/*<FaSearch className='a'/>*/}
                 </div>
             </div>
 
---
+++ src/component/summonerInfo/js/InfoTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../../header/js/Header";
+
+const InfoTemplate = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default InfoTemplate;
