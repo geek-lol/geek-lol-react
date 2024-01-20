c46a16b0eb47d1d7bbf2b79e3639c9aac1a96908
hyowon <dlgy9714@gmail.com>
2024-01-21 04:53:12 +0900

    헤더 수정중...

--- src/component/header/js/Header.js
+++ src/component/header/js/Header.js
@@ -1,16 +1,43 @@
 import React, {Component, useEffect, useState} from "react";
-import PropTypes from "prop-types";
 import "../scss/Header.scss";
-import { FaSearch } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
 import LoginBtn from "./LoginBtn";
 import SearchBox from "./SearchBox";
-import cn from 'classnames';
 import Profile from "./Profile";
 import MenuModal from "./MenuModal";
 import {Link} from "react-router-dom";
+import {GoChevronDown} from "react-icons/go";
+import cn from "classnames";
 
 const Header =()=> {
+  //nav 시작
+  const indicator = document.querySelector('.nav-indicator');
+  const items = document.querySelectorAll('.nav-item');
+  const [hovers,sethover]=useState(false);
+  const tldlqkf=()=>{
+    sethover(false);
+  }
+  const boardClickHandler=()=>{
+    sethover(!hovers);
+  }
+  function handleIndicator(el) {
+    items.forEach(item => {
+      item.classList.remove('is-active');
+      item.removeAttribute('style');
+    });
+    indicator.style.width = `${el.offsetWidth}px`;
+    indicator.style.left = `${el.offsetLeft}px`;
+    indicator.style.backgroundColor = el.getAttribute('active-color');
+
+    el.classList.add('is-active');
+    el.style.color = el.getAttribute('active-color');
+  }
+  items.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+      handleIndicator(e.target)});
+    item.classList.contains('is-active') && handleIndicator(item);
+  });
+  //nav 끝
   const [menu, setMenu]=useState(false);
   const [isInput,setIsInput]=useState(true);
   const [isProfile,setProfile]=useState(false);
@@ -30,7 +57,7 @@ const Header =()=> {
   }
   return (
       <div>
-        <header id="header">
+        <header id="header" >
           <nav id="nav-box">
             <div className="logo__box">
               <Link className="logo" to="/">
@@ -38,29 +65,38 @@ const Header =()=> {
               </Link>
             </div>
             <ul className="content__box">
-              <li className="content__menu">
-                <Link className='a' to="/">홈</Link>
-              </li>
-              <li className="content__menu">
-                <Link className='a' to="/rank">랭킹</Link>
-              </li>
-              <li className="content__menu">
-                <Link className='a' to="/board">게시판</Link>
-              </li>
-              <li className="content__menu">
-                <Link className='a' to="#">..</Link>
-              </li>
-              {isInput===true&&<SearchBox/>}
-
+              <Link to="/" className="nav-item is-active" active-color="orange">홈</Link>
+              <Link to="/rank" className="nav-item" active-color="green">랭킹</Link>
+              <div  className="board-btn nav-item" active-color="blue" onClick={boardClickHandler} >
+                게시판
+                <ul className={cn("hide-btn",{hovers:hovers})}>
+                    <li>
+                      <Link to="/board/main">자유게시판</Link>
+                    </li>
+                    <li>
+                      <Link to="/">LCK</Link>
+                    </li>
+                    <li>
+                      <Link to="/">공략게시판</Link>
+                    </li>
+                    <li>
+                      <Link to="/">하이라이트</Link>
+                    </li>
+                </ul>
+              </div>
+              <Link to="/" className="nav-item" active-color="red">미니게임</Link>
+              <Link to="/" className="nav-item" active-color="rebeccapurple">트롤사형투표</Link>
+              <span className="nav-indicator"></span>
+              {isInput === true && <SearchBox/>}
             </ul>
             <ul className="certification__box">
-              {isLogin===true&&<LoginBtn/>}
-              {isProfile===true&&<Profile/>}
+              {isLogin === true && <LoginBtn/>}
+              {isProfile === true && <Profile/>}
 
             </ul>
-              <div className="nav_toggle_Btn" onClick={menuHandler}>
-                <TiThMenu/>
-              </div>
+            <div className="nav_toggle_Btn" onClick={menuHandler}>
+              <TiThMenu/>
+            </div>
           </nav>
         </header>
         <MenuModal menu={menu} isProfile={isProfile} isLogin={isLogin}/>
