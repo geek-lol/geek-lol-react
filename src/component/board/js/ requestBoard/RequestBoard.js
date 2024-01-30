656a7bea06dd59ed27c4e9421ba30242b348c421
wonhwo <dlgy9714@gmail.com>
2024-01-30 17:48:40 +0900

    tq

--- src/component/board/js/ requestBoard/RequestBoard.js
+++ src/component/board/js/ requestBoard/RequestBoard.js
@@ -63,27 +63,10 @@ const RequestBoard = () => {
                             <p>누가누가 문제인가요?? ㅅㅂ?</p>
                         </div>
                         <div className="main-text2">
-                            <p>현제투표 글</p>
+                            <p>현재투표 글</p>
                         </div>
                     </div>
-                    <div className="prev hero">
-                        <div className='videoo'>
-                            <ReactPlayer
-                                url={"/assets/videos/lol.mp4"}
-                                width={'300px'}
-                                height={'100%'}/>
-                        </div>
-                        <div className="text"></div>
-                        <div className="logo">
-                            <img src={process.env.PUBLIC_URL + '/assets/lollogo.jpg'} alt=""/>
-                        </div>
-                        <div className="main-text">
-                            <p>누가누가 문제인가요?? ㅅㅂ?</p>
-                        </div>
-                        <div className="main-text2">
-                            <p>최고 추천 글</p>
-                        </div>
-                    </div>
+
                 </div>
 
                 <div className="board_list_box">
