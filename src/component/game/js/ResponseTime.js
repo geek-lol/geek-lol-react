ba626c8ef4d16e4515cb9f1368542fa09a6ee089
NoteSeonjin <hawooks112@gmail.com>
2024-02-09 17:36:50 +0900

    # 반응속도게임 - 디자인 변경

--- src/component/game/js/ResponseTime.js
+++ src/component/game/js/ResponseTime.js
@@ -11,7 +11,7 @@ import Paper from '@mui/material/Paper';
 import {getCurrentLoginUser} from "../../../utils/login-util";
 import axios from "axios";
 import {formatDate} from "../../../utils/format-date";
-
+import Poro from "../scss/poro.jpg";
 const ResponseTime = () => {
 
     let startTime; // 시작시간
@@ -129,45 +129,49 @@ const ResponseTime = () => {
     }
 
     return (
-        <>  
-            <div>
-                <h1 className="game-title"> 반응 속도 테스트 </h1>
-                <div id="gameCount">진행도 : 0/5</div>
+        <>
+        <div className="res-wrapper"> </div>
+        <div>
+            <h1 className="game-title"> 반응 속도 테스트 </h1>
+            <div id="gameCount" style={{color:"white",marginBottom:"20px"}}>진행도 : 0/5</div>
+            <div className="game-main">
                 <div id="screen" className="waiting" onClick={startClick}>클릭해서 시작하세요</div>
-                <div id="testReset" className="btn-reset non" onClick={resetButtonClickHandler}>
-                    <Button variant="contained">테스트 다시하기</Button>
-                </div>
                 <div id="result"></div>
             </div>
-            <TableContainer sx={{width:'65%', mx:'auto', mt:30, mb:30}} component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table" >
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">순위</TableCell>
-                            <TableCell align="left">닉네임(아이디)</TableCell>
-                            <TableCell align="left">반응 속도</TableCell>
-                            <TableCell align="left">날짜</TableCell>
+            <div id="testReset" className="btn-reset non" onClick={resetButtonClickHandler}>
+                <Button variant="contained">테스트 다시하기</Button>
+            </div>
+        </div>
+
+        <TableContainer sx={{width:'65%', mx:'auto', mt:30, mb:30}} component={Paper}>
+            <div className="rank-title"> 순위표 </div>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table" >
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="center">순위</TableCell>
+                        <TableCell align="left">닉네임(아이디)</TableCell>
+                        <TableCell align="left">반응 속도</TableCell>
+                        <TableCell align="left">날짜</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rankList.map((row,index) => (
+                        <TableRow
+                            key={row.gameId}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell align="center" component="th" scope="row">
+                                {index+1}
+                            </TableCell>
+                            <TableCell align="left">{`${row.userName}(${row.userId})`}</TableCell>
+                            <TableCell align="left">{row.score}</TableCell>
+                            <TableCell align="left">{formatDate(row.recordDate,null)}</TableCell>
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rankList.map((row,index) => (
-                            <TableRow
-                                key={row.gameId}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell align="center" component="th" scope="row">
-                                    {index+1}
-                                </TableCell>
-                                <TableCell align="left">{`${row.userName}(${row.userId})`}</TableCell>
-                                <TableCell align="left">{row.score}</TableCell>
-                                <TableCell align="left">{formatDate(row.recordDate,null)}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-     
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </>
     );
 };
 
