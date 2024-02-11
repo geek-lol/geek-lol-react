bdd695628e7b0a6c0ca195c884d71a154ffb7ed2
hyowon <109724865+wonhwo@users.noreply.github.com>
2024-02-12 01:40:54 +0900

    이것저것수정

--- src/component/header/js/Profile.js
+++ src/component/header/js/Profile.js
@@ -6,28 +6,27 @@ import {Link} from "react-router-dom";
 const Profile = props => {
     const [token, setToken] = useState(getCurrentLoginUser().token);
     const [imgUrl, setImgUrl] = useState(null);
+    const API_URL = "http://localhost:8686/user";
 
-    const getProfile=async ()=>{
-        await fetch(`/user/load-profile`,{
+    const userProfileFetch = async () =>{
+        const url = API_URL + "/load-profile";
+        const res = await fetch(url, {
+            method: 'GET',
             headers: {
-                Authorization: `Bearer ${token}`,
+                'Authorization': `Bearer ${token}`
             }
-        }).then(async res => {
-            if (res.status === 200) {
-                console.log("잘 옴");
-                const profileData = await res.blob();
-                // blob이미지를 url로 변환
-                const imgUrl = window.URL.createObjectURL(profileData);
-                console.log('imgUrl DB에서 넘어온거');
-                console.log(imgUrl);
-                setImgUrl(imgUrl);
-            }
-        })
+        });
+        if (res.status === 200) {
+            const profileData = await res.blob();
+
+            // blob이미지를 url로 변환
+            const imgUrl = window.URL.createObjectURL(profileData);
+            setImgUrl(imgUrl);
+        }
     }
     useEffect(() => {
-        getProfile();
-    }, []);
-
+        userProfileFetch();
+    }, [setImgUrl]);
     let src;
     src = require('../../../image/profile.jpg');
     return (
