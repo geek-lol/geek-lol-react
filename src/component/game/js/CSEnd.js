670fe5ff30b5c011d02fd16f86806f9a74247f6d
NoteSeonjin <hawooks112@gmail.com>
2024-02-17 14:38:07 +0900

    # cs게임 기타 수정

--- src/component/game/js/CSEnd.js
+++ src/component/game/js/CSEnd.js
@@ -31,14 +31,9 @@ class endScene extends Phaser.Scene {
         this.add.rectangle(700,400,500,400,0xFFFF00)
         this.score = data.score;
         this.add.text(570,300,`당신의 점수 ${this.score}`,{ font: '36px NeoDunggeunmo', fill: '#000000' })
-        const close = this.add.text(550,500,`랭킹보기`,{ font: '36px NeoDunggeunmo', fill: '#000000' });
-        close.setInteractive();
-        const home = this.add.text(750,500,`홈으로`,{ font: '36px NeoDunggeunmo', fill: '#000000' });
+        const home = this.add.text(630,500,`홈으로`,{ font: '36px NeoDunggeunmo', fill: '#000000' });
         home.setInteractive();
 
-        close.on('pointerdown',(pointer)=>{
-            this.scene.setVisible(false,'endScene');
-        })
         home.on('pointerdown', (pointer)=>{
             this.scene.stop();
             // this.scene.setVisible(false,'rankScene')
