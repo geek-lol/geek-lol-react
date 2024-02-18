1a7982535421ee92673d68915b58fb4400658e33
yongseopK <kk0@kakao.com>
2024-02-19 05:37:48 +0900

    흠.,

--- src/component/header/js/Profile.js
+++ src/component/header/js/Profile.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import '../scss/Profile.scss';
 import {getCurrentLoginUser} from "../../../utils/login-util";
@@ -6,28 +6,39 @@ import {Link} from "react-router-dom";
 import {USER_URL} from "../../../config/host-config";
 const Profile = (profile) => {
     const [token, setToken] = useState(getCurrentLoginUser().token);
-    const [imgUrl, setImgUrl] = useState(null);
+    const [imgUrl, setImgUrl] = useState(undefined);
     const API_URL = "http://localhost:8686/user";
 
     const userProfileFetch = async () =>{
-        const url = USER_URL + "/load-profile";
-        const res = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        if (res.status === 200) {
-            const profileData = await res.text();
+        setTimeout(async () => {
+            const url = USER_URL + "/load-profile";
+            const res = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            if (res.status === 200) {
+                const profileData = await res.text();
 
-            // blob이미지를 url로 변환
-            console.log(profileData);
-            setImgUrl(profileData);
-        }
+                // blob이미지를 url로 변환
+                console.log(profileData);
+                setImgUrl(profileData);
+            }
+        }, 200);
+
     }
+    const imgRef = useRef(null);
+
     useEffect(() => {
         userProfileFetch();
-    }, [setImgUrl,profile]);
+    }, [profile]);
+
+    useEffect(() => {
+        if (imgRef.current) {
+            imgRef.current.src = imgUrl;
+        }
+    }, [imgUrl]);
     let src;
     src = process.env.PUBLIC_URL + "/defaultUser.jpg";
     return (
